371cf16c42f4b31867a43a60a858d6ec676aa488
Richard Jonas <richard.jonas@wallapop.com>
2016-10-19 16:37:06 +0200

    Configure lager formatter

--- src/emock_rest_db.erl
+++ src/emock_rest_db.erl
@@ -1,7 +1,27 @@
 -module(emock_rest_db).
 
--export([execute/2]).
+-export([config/1,
+         execute/2]).
+
+config(Config) ->
+    lager:debug("~p", [Config]),
+    Config.
 
 execute(State, _Config) ->
+    #{request := Request} = State,
+    Method = cowboy_req:method(Request),
+    Path = cowboy_req:path(Request),
+    QS = cowboy_req:qs(Request),
+    Body = cowbow_req:body(Request),
+    lager:debug("~p ~p ~p", [Method, Path, QS, Body]),
     {ok, State}.
+
+handle_get(Resource) ->
+    ok.
+
+handle_get(Resource, Id) ->
+    ok.
+
+handle_post(Resource, Id, Body) ->
+    ok.
 
